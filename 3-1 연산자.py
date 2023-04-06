9fbc61f57250a2295b18be3c7961d205d80b8d19
deokone2 <129834799+deokone2@users.noreply.github.com>
2023-04-06 18:02:20 +0900

    Add files via upload

---
+++ 3-1 연산자.py
@@ -0,0 +1,26 @@
+print(1+1)#더하기
+print(3-2)#빼기
+print(5*2)#곱하기
+print(6/3)#나누기
+
+print(2**3)#제곱
+print(5%3)#나머지
+
+print(10%3)
+print(5//3)#나누었을때 몫
+
+print(10<3)#비교연산(<,>,,<=,>=)
+
+print(3==3)#똑같다
+print(3==4)
+print(3+3==6)
+
+print(1!=3)#같지 않다
+print(not(1!=3))
+
+print((3>0)and(3<5))#and 조건
+print((3>0)&(3<5))#and=&
+
+print ((3>0)or(3>5))#or=| 조건 (둘중하나만 만족해도 true)
+
+print(5>4>3)#연속되는값도 가능
---
+++ 3-4 랜덤함수.py
@@ -0,0 +1,32 @@
+from random import * # 랜덤함수 라이브러리
+
+# print(random())#1.0미만의 임의의 값 생성
+# print(random()*10)#10.0 미만의 임의의 수
+# print(int(random()*10))#10 미만의 임의의 수
+# print(int(random()*10))#10 미만의 임의의 수
+# print(int(random()*10))#10 미만의 임의의 수
+# print(int(random()*10+1))#1부터 10 이하의 임의의 수
+# print(int(random()*10+1))
+# print(int(random()*10+1))
+# print(int(random()*10+1))
+
+# print(int(random()*45+1))#로또
+# print(int(random()*45+1))
+# print(int(random()*45+1))
+# print(int(random()*45+1))
+# print(int(random()*45+1))
+# print(int(random()*45+1))
+
+# print(randrange(1,46))#1부터 46 미만의 값 생성
+# print(randrange(1,46))
+# print(randrange(1,46))
+# print(randrange(1,46))
+# print(randrange(1,46))
+# print(randrange(1,46))
+
+print(randint(1,45))#1부터 45 이하의 값 생성
+print(randint(1,45))
+print(randint(1,45))
+print(randint(1,45))
+print(randint(1,45))
+print(randint(1,45))
