a5fc7e9c2738c0ea33574279cc47910865a9d693
deokone2 <129834799+deokone2@users.noreply.github.com>
2023-04-06 18:00:56 +0900

    Delete 2023-04-04 동아리 1일차 directory

--- 2023-04-04 동아리 1일차/4-1 문자열.py
+++
@@ -1,7 +0,0 @@
-sentnce='나는 소년입니다'
-print(sentnce)
-sentence2="파이썬은 쉬워요"
-print(sentence2)
-sentence3='''나는 소년이고
-파이썬은 쉬워요'''
-print(sentence3)
--- 2023-04-04 동아리 1일차/퀴즈#2.py
+++
@@ -1,3 +0,0 @@
-from random import*
-
-print('오프라인 스터디 모임 날짜는 매월',randint(4,28),'일로 선정되었습니다')
